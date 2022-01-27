9598bb22c98effe255dd294acef2b4a9d007809a
Eduardo <eduardoarandah@gmail.com>
2022-01-27 13:53:52 -0600

    Add keywords section to content single

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -20,5 +20,14 @@
     {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
   </footer>
 
+  @if (has_tag())
+    <div class="mt-6 py-4 px-2 md:px-4 flex space-x-4 items-center bg-gray-200 rounded w-full shadow">
+      <h3 class="text-lg font-bold text-red-900 uppercase">
+        {{ __('Keywords', 'sage') }}
+      </h3>
+      <?php the_tags('<div class="flex items-center space-x-3 uppercase font-bold text-gray-700">', ' ', '</div>'); ?>
+    </div>
+  @endif
+
   @php(comments_template())
 </article>
