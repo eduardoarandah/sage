0a0251a9f85835e1b04afc6c69d7f36b9579959e
Eduardo <eduardoarandah@gmail.com>
2022-01-27 17:41:33 -0600

    Prevent block from showing if no posts

--- resources/views/post-widget-structures/demo.blade.php
+++ resources/views/post-widget-structures/demo.blade.php
@@ -1,12 +1,14 @@
-<div class="grid gap-4 md:grid-cols-3 lg:gap-8">
-  @foreach ($posts as $post)
-    <div itemscope itemtype="https://schema.org/NewsArticle">
-      <div itemprop="image">
-        <a href="{{ $post->permalink }}" rel="bookmark" title="{{ $post->post_title }}">{!! $post->rendered_thumbnail !!}</a>
+@if (count($posts))
+  <div class="grid gap-4 md:grid-cols-3 lg:gap-8">
+    @foreach ($posts as $post)
+      <div itemscope itemtype="https://schema.org/NewsArticle">
+        <div itemprop="image">
+          <a href="{{ $post->permalink }}" rel="bookmark" title="{{ $post->post_title }}">{!! $post->rendered_thumbnail !!}</a>
+        </div>
+        <div class="w-full py-3">
+          <a class="block mt-1 font-semibold leading-tight text-lg tracking-wide" href="{{ $post->permalink }}" rel="bookmark" title="{{ $post->post_title }}">{{ $post->post_title }}</a>
+        </div>
       </div>
-      <div class="w-full py-3">
-        <a class="block mt-1 font-semibold leading-tight text-lg tracking-wide" href="{{ $post->permalink }}" rel="bookmark" title="{{ $post->post_title }}">{{ $post->post_title }}</a>
-      </div>
-    </div>
-  @endforeach
-</div>
+    @endforeach
+  </div>
+@endif
